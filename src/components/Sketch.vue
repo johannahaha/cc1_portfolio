755df92d1e55d462ef215f362939b9885f4f5a7c
Johanna <>
2021-02-24 15:24:12 +0100

    window now completly in isClient

--- src/components/Sketch.vue
+++ src/components/Sketch.vue
@@ -31,17 +31,20 @@ export default {
     }),
     methods: {
         setup(s){
-            this.width =  window.innerWidth,
-            this.height = window.innerHeight,
-            this.mouseXLast= window.innerWidth/2,
-            this.mouseYLast= window.innerHeight/2,
-            s.createCanvas(this.width,this.height);
-            
-            s.background(255,255,255);
-            let c = s.color(217,164,4,10)
-            s.stroke(c);
-            s.noFill();
-            
+
+            if (process.isClient){
+                this.t=0;
+                this.width =  window.innerWidth,
+                this.height = window.innerHeight,
+                this.mouseXLast= window.innerWidth/2,
+                this.mouseYLast= window.innerHeight/2,
+                s.createCanvas(this.width,this.height);
+                
+                s.background(255,255,255);
+                let c = s.color(217,164,4,10)
+                s.stroke(c);
+                s.noFill();
+            }
 
             // document.addEventListener('resize', () => {
             //     console.log("resizing");
@@ -51,32 +54,35 @@ export default {
         },
 
         draw(s){
+            if (process.isClient){
+                let x1 = this.width * s.noise(this.t + 10);
+                let y1 = this.height * s.noise(this.t - 10);
+                let x2 = this.width * s.noise(this.t + 20);
+                let y2 = this.height * s.noise(this.t - 20);
+                let x3 = this.width * s.noise(this.t + 30);
+                let y3 = this.height * s.noise(this.t - 30);
+                let x4 = this.height* s.noise(this.t+100);
+                let y4 = this.width* s.noise(this.t+100);
+                let x5 = this.width * s.noise(this.mouseXLast-s.mouseX);
+                let y5 = this.height * s.noise(this.mouseYLast-s.mouseY);
 
-            let x1 = this.width * s.noise(this.t + 10);
-            let y1 = this.height * s.noise(this.t - 10);
-            let x2 = this.width * s.noise(this.t + 20);
-            let y2 = this.height * s.noise(this.t - 20);
-            let x3 = this.width * s.noise(this.t + 30);
-            let y3 = this.height * s.noise(this.t - 30);
-            let x4 = this.height* s.noise(this.t+100);
-            let y4 = this.width* s.noise(this.t+100);
-            let x5 = this.width * s.noise(this.mouseXLast-s.mouseX);
-            let y5 = this.height * s.noise(this.mouseYLast-s.mouseY);
+                s.bezier(s.mouseX,s.mouseY,x1, y1, x3, y3, x2, y2,x5,y5);
 
-            s.bezier(s.mouseX,s.mouseY,x1, y1, x3, y3, x2, y2,x5,y5);
+                this.t += 0.003;
+                this.mouseXLast = s.mouseX;
+                this.mouseYLast = s.mouseY;
 
-            this.t += 0.003;
-            this.mouseXLast = s.mouseX;
-            this.mouseYLast = s.mouseY;
-
-            if (this.t >= 100){
-                s.background(255,255,255);
-                this.t=0;
+                if (this.t >= 100){
+                    s.background(255,255,255);
+                    this.t=0;
+                }
             }
         },
 
         windowresized(s){
-            s.resizeCanvas(window.innerWidth, window.innerHeight);
+            if (process.isClient){
+                s.resizeCanvas(window.innerWidth, window.innerHeight);
+            }
         }
         //let myp5 = new P5(sketch, document.getElementById('p5sketch'));
     },
