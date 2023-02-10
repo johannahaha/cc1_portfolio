0d353fd747694320d6f483e4728e1bc55cb0d95b
johannahaha <johanna.hartmann@filmuniversitaet.de>
2023-02-10 09:52:11 +0100

    remove font import

--- src/main.js
+++ src/main.js
@@ -1,41 +1,32 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue';
-import '~/assets/main.scss';
+import DefaultLayout from "~/layouts/Default.vue";
+import "~/assets/main.scss";
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { config, library } from "@fortawesome/fontawesome-svg-core";
 
-import { faEye } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
-import "@fontsource/aileron"
-import "@fontsource/archivo-black"
+import "@fontsource/archivo-black";
 
-import "@fontsource/archivo/400.css"
+import "@fontsource/archivo/400.css";
 import "@fontsource/archivo/variable.css";
 
-import "@fontsource/koh-santepheap/700.css";
-import "@fontsource/koh-santepheap/900.css";
-
-
-import "@fontsource/koh-santepheap"
-
-config.autoAddCss = false; 
-
+config.autoAddCss = false;
 
 export default function (Vue, { router, head, isClient }) {
-  library.add(faGithub, faTwitter,faEye);
+   library.add(faGithub, faTwitter, faEye);
 
-  // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout);
-  Vue.component('font-awesome', FontAwesomeIcon);
+   // Set default layout as a global component
+   Vue.component("Layout", DefaultLayout);
+   Vue.component("font-awesome", FontAwesomeIcon);
 
-  head.link.push({
-    rel: 'stylesheet',
-    href: 'https://use.typekit.net/mix2txx.css'
-  });
-
+   head.link.push({
+      rel: "stylesheet",
+      href: "https://use.typekit.net/mix2txx.css",
+   });
 }
