7dfa3e39ae285f8d35c09f55af961beec4110f97
johannahaha <johanna.hartmann@filmuniversitaet.de>
2024-01-16 17:50:13 +0100

    seo and metadata

--- portfolio_nuxt/app.vue
+++ portfolio_nuxt/app.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
    <div>
       <NuxtLayout>
@@ -9,4 +5,15 @@
       </NuxtLayout>
    </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+   title: 'Johanna Hartmann',
+   ogTitle: 'Johanna Hartmann',
+   description: 'Portfolio website of Johanna Hartmann, a Creative Technologist, Web Developer and Data Visualization Designer & Developer based in Berlin.',
+   ogDescription: 'Portfolio website of Johanna Hartmann, a Creative Technologist, Web Developer and Data Visualization Designer & Developer based in Berlin.',
+   ogImage: 'https://img/klimakarten/klimakarten02.jpg',
+   twitterCard: 'summary_large_image',
+})
+</script>
 
